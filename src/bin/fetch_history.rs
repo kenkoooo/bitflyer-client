8e817a5afccda62438d821cdd75039c05034c230
kenkoooo <kenkou.n@gmail.com>
2019-08-30 13:11:20 +0900

    Refactoring to avoid API limit

--- src/bin/fetch_history.rs
+++ src/bin/fetch_history.rs
@@ -3,6 +3,8 @@ use rusqlite::Connection;
 use rusqlite::NO_PARAMS;
 use std::env;
 use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -24,19 +26,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let client = HttpBitFlyerClient::default();
     loop {
-        let history = client.fetch_history(current_minimum_id)?;
-        let tx = conn.transaction()?;
-        for history in history.iter() {
-            let json = serde_json::to_string(history)?;
-            tx.execute(
-                "INSERT INTO bit_history (id, json) values (?1, ?2)",
-                &[&history.id.to_string(), &json],
-            )?;
-        }
-        tx.commit()?;
+        match client.fetch_history(current_minimum_id) {
+            Ok(history) => {
+                let tx = conn.transaction()?;
+                for history in history.iter() {
+                    let json = serde_json::to_string(history)?;
+                    tx.execute(
+                        "INSERT INTO bit_history (id, json) values (?1, ?2)",
+                        &[&history.id.to_string(), &json],
+                    )?;
+                }
+                tx.commit()?;
 
-        let min = history.into_iter().map(|history| history.id).min().unwrap();
-        current_minimum_id = Some(min - 1);
-        eprintln!("{:?}", current_minimum_id);
+                let min = history.into_iter().map(|history| history.id).min().unwrap();
+                current_minimum_id = Some(min - 1);
+                eprintln!("{:?}", current_minimum_id);
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+                sleep(Duration::from_secs(1));
+            }
+        }
     }
 }
